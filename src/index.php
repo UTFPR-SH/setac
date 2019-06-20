34c63ab7b544c35d20a3c28a736e3726c016cf37
Mikael Messias <mikaelmessias@outlook.com>
2019-06-20 15:56:01 -0300

    Adiciona a página principal
    Ainda vazia, mas já deixa a estrutura básica do site pronta
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title>SeTAC² - Semana Tecnológica Acadêmica de Ciêcia da Computação</title>
+</head>
+<body>
+
+</body>
+</html>
