5928592efb768092b351b2e2605a5b4547a9b576
NataN-Dotti <ceolan@alunos.utfpr.edu.br>
2019-07-01 10:46:36 -0300

    Adiciona protótipo da classe EditionRegistration
    Signed-off-by: NataN-Dotti <ceolan@alunos.utfpr.edu.br>

---
+++ src/model/EditionRegistration.php
@@ -0,0 +1,15 @@
+<?php
+
+    class EditionRegistration{
+        public $id;
+        public $edition;
+        public $registrationDate;
+        public $checkIn;
+
+        public function __construct($_user, $_ edition){
+            
+        }
+
+    }
+
+?>
